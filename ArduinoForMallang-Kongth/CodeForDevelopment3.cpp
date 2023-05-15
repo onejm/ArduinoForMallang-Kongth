27b672552a326c4178c27829c14050b4c298c15b
onejm <jeongmin0324@gmail.com>
2023-05-15 23:38:03 +0900

    [ADD] 개발용코드3

---
+++ ArduinoForMallang-Kongth/CodeForDevelopment3.cpp
@@ -0,0 +1,12 @@
+//개발용 코드 
+void setup() { // setup안에 코드는 처음 한번만 실행됨
+	Serial.begin(9600);
+}
+
+void loop() { // loop안에 코드가 아두이노가 무한 반복으로 실행됨
+	for (int i = 50; i > 20; i--) {
+		Serial.print("g00");
+		Serial.print(i);
+		delay(500); //0.5초 정지 
+	}
+}
