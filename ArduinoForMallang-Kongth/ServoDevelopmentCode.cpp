dc35c04b273dd3d70564d4df9a3e97f44a9d4bd1
onejm <jeongmin0324@gmail.com>
2023-05-18 22:34:41 +0900

    [CHANGE] 데이터 수신 변경

---
+++ ArduinoForMallang-Kongth/ServoDevelopmentCode.cpp
@@ -0,0 +1,21 @@
+//개발용 코드 
+#include <Servo.h>
+Servo servo;
+int servoVal = 0;
+void setup() { // setup안에 코드는 처음 한번만 실행됨
+	Serial.begin(9600);
+	servo.attach(7);
+}
+
+void loop() { // loop안에 코드가 아두이노가 무한 반복으로 실행됨
+	if (Serial.available()) { //시리얼에 데이터가 입력됨
+		int in_data = Serial.read();
+		if (in_data == 1) {
+			servoVal += 90;
+			servo.write(servoVal);
+		}
+	}
+	Serial.print("h00");
+	Serial.print(12);
+	delay(500);
+}
