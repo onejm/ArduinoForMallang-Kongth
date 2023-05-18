203d8ae5c874c8328748122e487f7bcd7e814fab
onejm <jeongmin0324@gmail.com>
2023-05-18 21:16:44 +0900

    [CHANGE] 시리얼 출력 오류 고침

--- ArduinoForMallang-Kongth/CodeForDevelopment2.cpp
+++ ArduinoForMallang-Kongth/CodeForDevelopment2.cpp
@@ -5,12 +5,14 @@ void setup() { // setup안에 코드는 처음 한번만 실행됨
 
 void loop() { // loop안에 코드가 아두이노가 무한 반복으로 실행됨
 	for (int i = 50; i > 20; i--) {
-		Serial.print("w00" + i);
+		Serial.print("w00");
+		Serial.print(i);
 		delay(500); //0.5초 정지 
 	}
 
 	for (int i = 30; i > 10; i--) {
-		Serial.print("w00" + i);
+		Serial.print("w00");
+		Serial.print(i);
 		delay(500); //0.5초 정지 
 	}
 }
