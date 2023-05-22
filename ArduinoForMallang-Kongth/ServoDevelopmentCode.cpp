0f8bba5b2b27750a65bbd5bdd42c4620369ec5d4
onejm <jeongmin0324@gmail.com>
2023-05-23 00:25:14 +0900

    [ADD] 급식기 테스트 코드

--- ArduinoForMallang-Kongth/ServoDevelopmentCode.cpp
+++ ArduinoForMallang-Kongth/ServoDevelopmentCode.cpp
@@ -8,14 +8,16 @@ void setup() { // setup안에 코드는 처음 한번만 실행됨
 }
 
 void loop() { // loop안에 코드가 아두이노가 무한 반복으로 실행됨
-	if (Serial.available()) { //시리얼에 데이터가 입력됨
-		int in_data = Serial.read();
-		if (in_data == 1) {
-			servoVal += 90;
-			servo.write(servoVal);
+	for (int i = 50; i > 20; i--) {
+		Serial.print("h00");
+		Serial.print(i);
+		if (Serial.available()) { //시리얼에 데이터가 입력됨
+			int in_data = Serial.read();
+			if (in_data == 1) {
+				servoVal += 90;
+				servo.write(servoVal);
+			}
 		}
+		delay(500); //0.5초 정지 
 	}
-	Serial.print("h00");
-	Serial.print(12);
-	delay(500);
 }
