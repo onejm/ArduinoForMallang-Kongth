87731fa8ae94df555f1de3dc3cb7789f147ba4b8
onejm <jeongmin0324@gmail.com>
2023-05-25 23:48:25 +0900

    [ADD] 급수기 코드

--- ArduinoForMallang-Kongth/ServoDevelopmentCode.cpp
+++ ArduinoForMallang-Kongth/ServoDevelopmentCode.cpp
@@ -1,23 +1,30 @@
 //개발용 코드 
 #include <Servo.h>
 Servo servo;
-int servoVal = 0;
+int angle = 90;
 void setup() { // setup안에 코드는 처음 한번만 실행됨
-	Serial.begin(9600);
-	servo.attach(7);
+    Serial.begin(9600);
+    servo.attach(9);
 }
 
 void loop() { // loop안에 코드가 아두이노가 무한 반복으로 실행됨
-	for (int i = 50; i > 20; i--) {
-		Serial.print("h00");
-		Serial.print(i);
-		if (Serial.available()) { //시리얼에 데이터가 입력됨
-			int in_data = Serial.read();
-			if (in_data == 1) {
-				servoVal += 90;
-				servo.write(servoVal);
-			}
-		}
-		delay(500); //0.5초 정지 
-	}
+    for (int i = 40; i > 20; i--) {
+        Serial.print("h00");
+        Serial.print(i);
+        if (Serial.available() > 0) {  // 시리얼 버퍼에 데이터가 있는지 확인
+            int input = Serial.parseInt();  // 입력된 정수 값을 읽어옴
+
+            if (input == 1) {  // 입력 값이 1인 경우
+                angle = 180;  // 서보 모터 각도를 90도로 설정
+                servo.write(angle);  // 서보 모터를 지정한 각도로 움직임
+                delay(500);  // 0.5초 대기
+            }
+            if (input == 2) {  // 입력 값이 1인 경우
+                angle = 0;  // 서보 모터 각도를 90도로 설정
+                servo.write(angle);  // 서보 모터를 지정한 각도로 움직임
+                delay(500);  // 0.5초 대기
+            }
+        }
+    }
+    delay(500); //0.5초 정지 
 }
---
+++ ArduinoForMallang-Kongth/footBowl.cpp
@@ -0,0 +1,56 @@
+//개발용 코드 
+#include <Servo.h>
+#include "HX711.h"
+#define calibration_factor 527.5 //캘리브레이션 값
+#define DOUT  4 //데이터 핀
+#define CLK  3 // 클럭 핀
+Servo servo;
+HX711 scale(DOUT, CLK);
+int value;
+int value2;
+int input = 0;
+int angle = 90;
+void setup() { // setup안에 코드는 처음 한번만 실행됨
+    Serial.begin(9600);
+    servo.attach(9);
+    scale.set_scale(calibration_factor);
+    scale.tare(); //영점잡기. 현재 측정값을 0으로 둔다.
+}
+
+void loop() { // loop안에 코드가 아두이노가 무한 반복으로 실행됨
+    value = (int)scale.get_units();
+    if (value <= 2) {
+        Serial.print("h");
+        Serial.print("0000");
+    }
+    else if (value > 2 && value <= 9) {
+        Serial.print("h");
+        Serial.print("000");
+        Serial.print(value);
+    }
+    else if (value > 9 && value <= 99) {
+        Serial.print("h");
+        Serial.print("00");
+        Serial.print(value);
+    }
+    else if (value > 99 && value <= 999) {
+        Serial.print("h");
+        Serial.print("0");
+        Serial.print(value);
+    }
+    else {
+        Serial.print("h");
+        Serial.print(value);
+    }
+    if (Serial.available() > 0 && input == 0) {  // 시리얼 버퍼에 데이터가 있는지 확인
+        input = Serial.parseInt();  // 입력된 정수 값을 읽어옴
+        value2 = value;
+    }
+    if (input > value - value2 && input != 0) {
+        servo.write(180);  // 서보 모터를 지정한 각도로 움직임
+    }
+    else if (input < value - value2 && input != 0) {
+        servo.write(90);
+        input = 0;
+    }
+}
